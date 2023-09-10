c0f080c2c13c9dc0d795879533044c25a5634069
Andrey <Andrey@mail.ru>
2023-09-10 13:31:39 +0300

    Решённые задачи

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,46 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] arr = new int[size];
+Random rnd = new Random();
+
+for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int FindEvenNumbers(int[] arr)
+{
+    int sum = 0;
+    for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] % 2 == 0) sum++;
+        }
+    return sum;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите размер массива");
+int Size = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = CreateArrayRndInt(Size, 100, 999);
+PrintArray(array);
+Console.WriteLine();
+
+int count = FindEvenNumbers(array);
+Console.WriteLine($"Количество четных значений в массиве составляет {count}");
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,59 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+using System.Reflection;
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+double[] arr = new double[size];
+Random rnd = new Random();
+
+for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]:F1}, ");
+        else Console.Write($"{arr[i]:F1}");
+    }
+    Console.Write("]");
+}
+
+double FindMaxNumbers(double[] arr)
+{
+    double max = 0;
+    for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] > max) max = arr [i];
+        }
+    return max;
+}
+
+double FindMinNumbers(double[] arr)
+{
+    double min = arr[0];
+    for(int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i] < min) min = arr [i];
+        }
+    return min;
+}
+
+Console.WriteLine("Введите размер массива");
+int Size = Convert.ToInt32(Console.ReadLine());
+
+double[] array = CreateArrayRndDouble(Size, 0, 100);
+PrintArrayDouble(array);
+Console.WriteLine();
+
+double minnumbers = FindMinNumbers(array);
+double maxnumbers = FindMaxNumbers(array);
+Console.WriteLine($"Разнциа минимального и максимального значения в массиве составляет {maxnumbers - minnumbers:F1}");
